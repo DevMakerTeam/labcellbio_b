eff941c057a1e90f6448c7d209d8d541fc1c9e7c
King <zaqrhkd2@naver.com>
2025-08-07 00:28:31 +0900

    ✨ feat: cors labcellbio 추가

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   // ✅ CORS 설정
   app.enableCors({
     // origin: true, // 또는 'http://localhost:3000'
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000', 'https://labcellbio.vercel.app'],
     credentials: true, // ✔️ 이거 중요!
   });
 
